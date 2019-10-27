4ebd5ebab2a0d568d6e743c9ff66080438c31fb7
Ethan-Clark <clark95@nsuok.edu>
2019-10-27 02:00:07 -0500

    Ball Game

---
+++ Ball Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Text countText;
+    public float speed;
+    public Text WinText;
+    private Rigidbody rb;
+    private int count;
+    float m = BallSpeedSlider.speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetcountText();
+        WinText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * m, 0.0f, moveVertical * m);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetcountText();
+            if (count >= 8)
+            {
+                WinText.text = "You Win!";
+            }
+        }
+    }
+    void SetcountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ Ball Game/Assets/Scripts/ShowData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowData : MonoBehaviour
+{
+  
+    public InputField PlayerNameInput;
+
+    public Text rounds;
+    public Text ballSpeed;
+    public Text objectSpeed;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+     
+        rounds.text = "Rounds: " + PlayerPrefs.GetInt("rounds");
+        ballSpeed.text = "Ball Speed: " + PlayerPrefs.GetFloat("BallSpeed");
+        objectSpeed.text = "Cube Speed: " + PlayerPrefs.GetFloat("SliderValue");
+
+    }
+    
+}
